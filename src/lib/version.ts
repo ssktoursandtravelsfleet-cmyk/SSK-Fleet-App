cf3ab47bcc1ba98f022817c999529626d36fea55
SSK Fleet <ssktoursandtravelsfleet@gmail.com>
2026-08-01 08:41:15 +0530

    chore: bump application version to 1.0.5

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,4 +1,4 @@
 import packageJson from "../../package.json";
 
-export const APP_VERSION = packageJson.version || "1.0.4";
+export const APP_VERSION = packageJson.version || "1.0.5";
 export const DISPLAY_VERSION = `Version ${APP_VERSION}`;
